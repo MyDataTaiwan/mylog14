6288e8dc8584176555c2fcfc83867ec86a19fead
BlackSquirrelz <tobias.weisskopf.ch@gmail.com>
2020-03-28 12:55:48 +0100

    multiLang
    Support for Mandarin and English

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,26 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+export function LanguageLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, TranslateModule.forRoot({
+    loader:{
+      provide: TranslateLoader,
+      useFactory: (LanguageLoader),
+      deps: [HttpClient]
+      }
+    })
+],
   providers: [
     StatusBar,
     SplashScreen,
@@ -21,3 +37,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
