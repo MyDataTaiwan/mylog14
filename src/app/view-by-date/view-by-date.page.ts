087f0a9401bc80f402dc1207a9c5087758ae61ae
James Chien <shchien@dt42.io>
2020-03-30 03:38:36 +0800

    Create page mockup for view-by-date page

--- src/app/view-by-date/view-by-date.page.ts
+++ src/app/view-by-date/view-by-date.page.ts
@@ -6,9 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-by-date.page.scss'],
 })
 export class ViewByDatePage implements OnInit {
-
+  items = [
+    {
+      day: 1,
+      month: '03',
+      date: '30',
+      bt: 37.9,
+      // Free-to-use mock image from https://pixabay.com/photos/cat-surprised-eyes-cat-s-eyes-2886062/
+      imgSrc: 'https://cdn.pixabay.com/photo/2017/10/24/20/33/cat-2886062_1280.jpg',
+      imgHeight: 300,
+    },
+    {
+      day: 0,
+      month: '03',
+      date: '29',
+      bt: 37.3,
+      // Free-to-use mock image from https://pixabay.com/photos/sleeping-dog-street-dog-animal-2837631/
+      imgSrc: 'https://cdn.pixabay.com/photo/2017/10/10/15/38/sleeping-dog-2837631_1280.jpg',
+      imgHeight: 400,
+    },
+  ];
   constructor() { }
-
   ngOnInit() {
   }
 
