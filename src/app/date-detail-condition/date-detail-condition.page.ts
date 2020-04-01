85e3485fc30dc2a3452ba55a0f24b3a466e41065
James Chien <shchien@dt42.io>
2020-04-01 14:57:07 +0800

    Add missing variables for ion-toggle property binding

--- src/app/date-detail-condition/date-detail-condition.page.ts
+++ src/app/date-detail-condition/date-detail-condition.page.ts
@@ -6,7 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-detail-condition.page.scss'],
 })
 export class DateDetailConditionPage implements OnInit {
-
+  cough = false;
+  fever = false;
+  runnyNose = false;
+  stuffyNose = false;
   constructor() { }
 
   ngOnInit() {
