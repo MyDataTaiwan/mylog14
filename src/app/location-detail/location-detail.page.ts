825e86a121195ea18cb833f049831dbbd74e70ed
DaYuan <sduum82@gmail.com>
2020-04-02 17:01:14 +0800

    add icons & 部分表單樣式

--- src/app/location-detail/location-detail.page.ts
+++ src/app/location-detail/location-detail.page.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location-detail.page.scss'],
 })
 export class LocationDetailPage implements OnInit {
+  isShow:true;
+  num = ["1", "2", 3, 4, 5, 6, 7];
   days = [5, 4, 3, 2, 1];
   constructor() { }
 
