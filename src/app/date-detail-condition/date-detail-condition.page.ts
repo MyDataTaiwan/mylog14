d21c62b337ee8b6b80b9adf54653f4ed0ab0302c
DaYuan <sduum82@gmail.com>
2020-04-03 07:31:57 +0800

    fix date-detail-condition ui

--- src/app/date-detail-condition/date-detail-condition.page.ts
+++ src/app/date-detail-condition/date-detail-condition.page.ts
@@ -6,6 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./date-detail-condition.page.scss'],
 })
 export class DateDetailConditionPage implements OnInit {
+  data={
+    cough:{
+      isShow:true,
+      detail:"5分鐘咳了10次左右"
+    },
+    fever:{
+      isShow:false,
+      detail:"5分鐘咳了10次左右"
+
+    },
+    runnyNose:{
+      isShow:false,
+      detail:"5分鐘咳了10次左右"
+
+    },
+    stuffyNose:{
+      isShow:true,
+      detail:"有點嗅覺失靈"
+
+    }
+  }
   cough = false;
   fever = false;
   runnyNose = false;
