daffac1209d9b265dfd0162e51718a6692389771
DaYuan <sduum82@gmail.com>
2020-04-03 03:13:27 +0800

    fix location-detail list 動態產生

--- src/app/location-detail/location-detail.page.ts
+++ src/app/location-detail/location-detail.page.ts
@@ -7,6 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationDetailPage implements OnInit {
   isShow:true;
+list=[
+  {
+    date:"2020/03/28",
+    day:1,
+    isShhow:true,
+    location:[
+      {
+        latitude:25.060018,
+        longitude:121.5307651,
+        time:'09:11'
+      },{
+        latitude:25.060018,
+        longitude:121.5307651,
+        time:'09:11'
+      }
+    ]
+  },
+   {
+    date:"2020/03/29",
+    day:2,
+    isShhow:true,
+    location:[
+      {
+        latitude:25.060018,
+        longitude:121.5307651,
+        time:'09:11'
+      },{
+        latitude:25.060018,
+        longitude:121.5307651,
+        time:'09:11'
+      }
+    ]
+  },  {
+    date:"2020/03/30",
+    day:3,
+    isShhow:true,
+    location:[
+      {
+        latitude:25.060018,
+        longitude:121.5307651,
+        time:'09:11'
+      },{
+        latitude:25.060018,
+        longitude:121.5307651,
+        time:'09:11'
+      }
+    ]
+  },
+]
+arry=[this.list[0],this.list[1],this.list[2],];
   num = ["1", "2", 3, 4, 5, 6, 7];
   days = [5, 4, 3, 2, 1];
   constructor() { }
@@ -15,3 +65,6 @@ export class LocationDetailPage implements OnInit {
   }
 
 }
+// location
+// longitude
+// latitude
