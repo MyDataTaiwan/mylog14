1418cc9d0ea2f775fb0f6b0a938fed289cab0e4e
DaYuan <sduum82@gmail.com>
2020-04-03 04:37:43 +0800

    location-detail 完成選擇功能與ui
    location-detail.page state & data 需要調整，目前缺少資料與分享功能

--- src/app/location-detail/location-detail.page.ts
+++ src/app/location-detail/location-detail.page.ts
@@ -6,62 +6,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./location-detail.page.scss'],
 })
 export class LocationDetailPage implements OnInit {
-  isShow:true;
-list=[
-  {
-    date:"2020/03/28",
-    day:1,
-    isShhow:true,
-    location:[
-      {
-        latitude:25.060018,
-        longitude:121.5307651,
-        time:'09:11'
-      },{
-        latitude:25.060018,
-        longitude:121.5307651,
-        time:'09:11'
-      }
-    ]
-  },
-   {
-    date:"2020/03/29",
-    day:2,
-    isShhow:true,
-    location:[
-      {
-        latitude:25.060018,
-        longitude:121.5307651,
-        time:'09:11'
-      },{
-        latitude:25.060018,
-        longitude:121.5307651,
-        time:'09:11'
-      }
-    ]
-  },  {
-    date:"2020/03/30",
-    day:3,
-    isShhow:true,
-    location:[
-      {
-        latitude:25.060018,
-        longitude:121.5307651,
-        time:'09:11'
-      },{
-        latitude:25.060018,
-        longitude:121.5307651,
-        time:'09:11'
-      }
-    ]
-  },
-]
-arry=[this.list[0],this.list[1],this.list[2],];
+  isShow: true;
+  isSelect: true;
+  isSelectlist = [];
+  list = [
+    {
+      date: "2020/03/28",
+      day: 1,
+      isShhow: true,
+      isSelect: false,
+      location: [
+        {
+          latitude: 25.060018,
+          longitude: 121.5307651,
+          time: '09:11'
+        }, {
+          latitude: 25.060018,
+          longitude: 121.5307651,
+          time: '09:11'
+        }
+      ]
+    },
+    {
+      date: "2020/03/29",
+      day: 2,
+      isShhow: true,
+      isSelect: false,
+      location: [
+        {
+          latitude: 25.060018,
+          longitude: 121.5307651,
+          time: '09:11'
+        }, {
+          latitude: 25.060018,
+          longitude: 121.5307651,
+          time: '09:11'
+        }
+      ]
+    }, {
+      date: "2020/03/30",
+      day: 3,
+      isShhow: true,
+      isSelect: false,
+      location: [
+        {
+          latitude: 25.060018,
+          longitude: 121.5307651,
+          time: '09:11'
+        }, {
+          latitude: 25.060018,
+          longitude: 121.5307651,
+          time: '09:11'
+        }
+      ]
+    },
+  ]
+  arry = [this.list[0], this.list[1], this.list[2],];
   num = ["1", "2", 3, 4, 5, 6, 7];
   days = [5, 4, 3, 2, 1];
   constructor() { }
 
   ngOnInit() {
+  }
+  addSelect(data) {
+    data.isSelect=!data.isSelect;
+    this.isSelectlist.push(data);
+    console.log("isSelectlist", this.isSelectlist)
+  }
+  delSelect(data) {
+    data.isSelect=!data.isSelect;
+    console.log("isSelectlist", this.isSelectlist)
+    var index = this.isSelectlist.indexOf(data);
+    if (index > - 1) {
+      this.isSelectlist.splice(index, 1);
+    }
+    console.log("isSelectlist", this.isSelectlist)
   }
 
 }
