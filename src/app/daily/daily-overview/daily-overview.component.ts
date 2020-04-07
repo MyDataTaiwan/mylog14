4a202ecbbece19f841097c72ea204f632b68be44
DaYuan <sduum82@gmail.com>
2020-04-07 17:25:39 +0800

    wip 控制動畫部分播放

--- src/app/daily/daily-overview/daily-overview.component.ts
+++ src/app/daily/daily-overview/daily-overview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { AnimationItem } from 'lottie-web';
-import { AnimationOptions } from 'ngx-lottie';
+import { AnimationOptions, } from 'ngx-lottie';
+import { promise } from 'protractor';
 @Component({
   selector: 'app-daily-overview',
   templateUrl: './daily-overview.component.html',
@@ -91,8 +92,8 @@ export class DailyOverviewComponent implements OnInit {
   // TimeArry:{
   //   [616, 674], [125, 184], [195, 246], [254, 306]
   // }
-  
-  arry: [number, number][] = [[35, 36], [125, 184], [195, 246], [254, 306],[313,365],[372,420],[436,490],[494,548],[555,611],[613,670],[677,734],[740,792],[797,850],[860,902],[952,922],[999,1200]];
+
+  arry: [number, number][] = [[35, 36], [125, 184], [195, 246], [254, 306], [313, 365], [372, 420], [436, 490], [494, 548], [555, 611], [613, 670], [677, 734], [740, 792], [797, 850], [860, 902], [952, 922], [999, 1200]];
   // arry: [number, number][] = [[35, 36], [125, 184], [125, 246], [125, 306],[125,365],[125,420],[125,490],[125,548],[125,611],[125,670],[125,734],[125,792],[125,850],[125,902],[125,922],[999,1200]];
 
   options: AnimationOptions = {
@@ -100,32 +101,75 @@ export class DailyOverviewComponent implements OnInit {
     path: '/assets/MyLog14BBT.json',
   };
   private animationItem: AnimationItem;
+  private isAnimationCreated: boolean = false;
 
   constructor(private ngZone: NgZone) { }
 
   ngOnInit() {
     // this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[this.items.length],true));
-    this.day("12132");
 
   }
-  ngOnChanges()	{
+  ngOnChanges() {
   }
+  //魔改可以觸發 animationItem.playSegments(this.arry[this.items.length], false);
+  //魔改“不”可以觸發 animationItem.playSegments(this.arry[this.items.length], true);
 
-  animationCreated(animationItem: AnimationItem): void {
+  AC($event) {
+    var temp;
+    //  temp = this.animationCreated($event);
+    temp;
+    this.animationCreated($event);
+    this.today();
+    return temp;
+  }
+  animationCreated(animationItem: AnimationItem) {
+    console.log("01" + this.isAnimationCreated);
+
     this.animationItem = animationItem;
-   
-
+    this.isAnimationCreated = true;
+    console.log("02" + this.isAnimationCreated);
+    console.log("is animation");
+    // 原本的在上，想要的在下
     console.log(animationItem);
     console.log("is animation");
-
-    animationItem.playSegments(this.arry[this.items.length],true);
-    this.ngZone.runOutsideAngular(() => animationItem.playSegments(this.arry[this.items.length],true));
-
-        this.day("12132");
+    console.log("03" + this.ngZone.runOutsideAngular);
 
 
+    this.ngZone.runOutsideAngular(() => {
+      animationItem.playSegments(this.arry[this.items.length], true)
+      console.log("04" + animationItem.playSegments(this.arry[this.items.length], false));
+    })
+
+    // animationItem.playSegments(this.arry[this.items.length], true);
+    // this.ngZone.runOutsideAngular(() => animationItem.playSegments(this.arry[this.items.length], true));
   }
-  
+  async today(){
+var data = this.items.length;
+    await this.ngZone.runOutsideAngular(() =>this.animationItem.playSegments(this.arry[data], false))
+  }
+
+  //可以用btn觸發的的
+  async day(data) {
+    await new Promise((resolve) => {
+      var timer = setInterval(() => {
+        if (this.isAnimationCreated) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 1);
+    });
+    console.log(this.isAnimationCreated);
+
+    // this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[1], true));
+    console.log("to day animation" + "dfdfd");
+
+    this.ngZone.runOutsideAngular(() =>
+      this.animationItem.playSegments(this.arry[this.items.length], true))
+    // let days = this.items.length - 1;
+    // this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[1], true));
+    // console.log("to day animation" + data);
+  }
+
 
   stop(): void {
     this.ngZone.runOutsideAngular(() => this.animationItem.stop());
@@ -143,21 +187,15 @@ export class DailyOverviewComponent implements OnInit {
     this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[1], true));
   }
   day2(): void {
-    this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[2],true));
+    this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[2], true));
   }
   day3(): void {
-    this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[3],true));
+    this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[3], true));
   }
   day15(): void {
-    this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[15],true));
+    this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[15], true));
   }
 
-  day(data) {
-    let days=this.items.length
-    this.ngZone.runOutsideAngular(() => this.animationItem.playSegments(this.arry[days],true));
-    console.log("to day animation"+data);
-
-  }
 
 
   pause() {
