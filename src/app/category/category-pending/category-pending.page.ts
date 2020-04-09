58e85b757059d7a972366f4f79b5a5c4b7b27180
DaYuan <sduum82@gmail.com>
2020-04-10 00:35:00 +0800

    增加篩選當日照片功能＆改善照片資訊欄位

--- src/app/category/category-pending/category-pending.page.ts
+++ src/app/category/category-pending/category-pending.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { PhotoService } from 'src/app/core/services/photo.service';
-import { ModalController,PopoverController } from '@ionic/angular';
+import { ModalController, PopoverController } from '@ionic/angular';
 import { CategorizeFinishPage } from './categorize-finish/categorize-finish.page';
 import { CategorizeImgPopoverPage } from './categorize-img-popover/categorize-img-popover.page';
 
@@ -12,41 +12,60 @@ import { CategorizeImgPopoverPage } from './categorize-img-popover/categorize-im
 })
 
 export class CategoryPendingPage implements OnInit, AfterViewInit {
-  
-  isSelect= false;
+
+  isSelect = false;
   catPic = {
     // Free-to-use mock image from https://pixabay.com/photos/cat-surprised-eyes-cat-s-eyes-2886062/
     src: 'https://cdn.pixabay.com/photo/2017/10/24/20/33/cat-2886062_1280.jpg',
   };
-  dogPic= {
+  dogPic = {
     // Free-to-use mock image from https://pixabay.com/photos/sleeping-dog-street-dog-animal-2837631/
     src: 'https://cdn.pixabay.com/photo/2017/10/10/15/38/sleeping-dog-2837631_1280.jpg',
   };
   pics = [
-   [this.catPic, this.dogPic, this.catPic],
-   [this.dogPic, this.catPic, this.dogPic],
-   [this.catPic, this.dogPic, this.catPic],
-   [this.dogPic, this.catPic, this.dogPic],
-   [this.catPic, this.dogPic, this.catPic],
-   [this.dogPic, this.catPic, this.dogPic],
+    [this.catPic, this.dogPic, this.catPic],
+    [this.dogPic, this.catPic, this.dogPic],
+    [this.catPic, this.dogPic, this.catPic],
+    [this.dogPic, this.catPic, this.dogPic],
+    [this.catPic, this.dogPic, this.catPic],
+    [this.dogPic, this.catPic, this.dogPic],
   ];
-  dataReturned:any;
-
+  dataReturned: any;
+  // photoList:any;
   constructor(
     public popoverController: PopoverController,
     public modalController: ModalController,
     public photoService: PhotoService,
   ) { }
- 
+
 
   ngOnInit() {
+    // this.photoList=this.photoService.photos$;
+    // console.log("photoList"+this.photoList.photos$[0].webviewPath)
   }
 
   ngAfterViewInit() {
+    // arry=photo.filter(index=>
+    //   new Date(index.timestamp).getUTCMonth()==new Date().getUTCMonth()&&
+    //   new Date(index.timestamp).getUTCDate()==new Date().getUTCDate()
+    //   )
+  }
+
+  toDayPhotos(photo) {///篩選當日照片
+    // console.log("photoList" + photo.snapshot.timestamp);
+    if (new Date(parseInt(photo.snapshot.timestamp,10)).getUTCMonth() == new Date().getUTCMonth() &&new Date(parseInt(photo.snapshot.timestamp,10)).getUTCDate() == new Date().getUTCDate()){
+      // if (new Date(parseInt(photo.snapshot.timestamp,10)).getUTCMonth() == new Date().getUTCMonth() ){
+        // console.log("photoY" + photo.snapshot.path);
+      return true;
+    }else{
+      // console.log("photoN" + photo.snapshot.path);
+      return false;
+    }
+
   }
 
   onClickShowImageSnapshot(photo) {
-    alert(photo.snapshot.timestamp+"|"+photo.snapshot.locationStamp.latitude+"|"+photo.snapshot.locationStamp.longitude);
+    alert(photo.snapshot.timestamp + "|" + photo.snapshot.locationStamp.latitude + "|" + photo.snapshot.locationStamp.longitude);
     console.log(photo.snapshot);
     /* Example photo.snapshot:
     {
@@ -84,12 +103,14 @@ export class CategoryPendingPage implements OnInit, AfterViewInit {
 
         "paramID": 123,
         "paramTitle": "Test Title",
-        "timestamp":photo.snapshot.timestamp,
-        "latitude":photo.snapshot.locationStamp.latitude,
-        "longitude":photo.snapshot.locationStamp.longitude,
-        "webviewPath":photo.webviewPath
+        "timestamp": photo.snapshot.timestamp,
+        "time": new Date(parseInt(photo.snapshot.timestamp,10)),
+        "latitude": photo.snapshot.locationStamp.latitude,
+        "longitude": photo.snapshot.locationStamp.longitude,
+        "webviewPath": photo.webviewPath
       }
     });
+    console.log(photo.snapshot)
     return await popover.present();
   }
   async openModal() {
