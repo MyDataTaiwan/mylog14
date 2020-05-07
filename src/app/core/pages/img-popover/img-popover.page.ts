ec5c08239014df801abad55b3311e67cf59c5d09
DaYuan <sduum82@gmail.com>
2020-05-07 22:20:54 +0800

    fix  Known Issue  by flow
    畫面會停留在照片預覽的頁面 沒有收掉

--- src/app/core/pages/img-popover/img-popover.page.ts
+++ src/app/core/pages/img-popover/img-popover.page.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { PopoverController, ModalController } from '@ionic/angular';
 import { Observable, of, concat, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { tap, map, catchError, takeUntil } from 'rxjs/operators';
 import { Record } from '../../interfaces/record';
 import { Photo } from '../../interfaces/photo';
 import { PhotoService } from '../../services/photo.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-img-popover',
@@ -17,6 +18,7 @@ export class ImgPopoverPage implements OnInit, OnDestroy {
   @Input() photo: Photo;
   address$: Observable<string>;
   destroy$ = new Subject();
+  localError:string;
   geocodeBaseUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=';
   geocodePostfix = '&language=zh-TW&key=AIzaSyC8Yg8Ig6VEZIWz8cWH3yfYOjAGzqIpDMI'; // FIXME: Shouldn't expose the key
 
@@ -25,10 +27,15 @@ export class ImgPopoverPage implements OnInit, OnDestroy {
   constructor(
     private httpClient: HttpClient,
     private popoverCtrl: PopoverController,
+    private modalCtrl: ModalController,
     private photoService: PhotoService,
+    private translateService: TranslateService,
+
+    
   ) { }
 
   ngOnInit() {
+
     // const url = this.geocodeBaseUrl + this.photo.locationStamp.latitude + ',' + this.photo.locationStamp.longitude + this.geocodePostfix;
     const url = this.OSMgeocodeBaseUrl + this.photo.locationStamp.latitude + ',' + this.photo.locationStamp.longitude + this.OSMgeocodePostfix;
     this.address$ = this.httpClient.get(url)
@@ -39,12 +46,15 @@ export class ImgPopoverPage implements OnInit, OnDestroy {
         // tap((res: GeocodingResponse) => console.log("res4", res[0])),
         // tap(() => console.log("res3", this.address$)),
         // map((res: GeocodingResponse) => res.results[0].formatted_address),
-        map((res: GeocodingResponse) => res[0].address.state+","+res[0].address.suburb),
+        map((res: GeocodingResponse) => res[0].address.state + "," + res[0].address.suburb),
         // map((res: GeocodingResponse) => res[0].display_name),
         tap(() => console.log("res", this.address$)),
+        // catchError(() => of(this.localError)),
         catchError(() => of('無法取得地址資訊')),
       );
   }
+
+ 
 
   ngOnDestroy(): void {
     this.destroy$.next();
@@ -52,19 +62,26 @@ export class ImgPopoverPage implements OnInit, OnDestroy {
   }
 
   cancel() {
-    return this.popoverCtrl.dismiss();
+    // return this.popoverCtrl.dismiss();
+    return this.popoverCtrl.dismiss().then(() => {
+      this.modalCtrl.dismiss({
+        'dismissed': true
+      });
+    })
   }
 
   confirm() {
     console.log(this.record);
     concat(
       this.photoService.deletePhoto(this.record, this.photo),
-      this.popoverCtrl.dismiss(),
-    )
-      .pipe(
+      this.popoverCtrl.dismiss().then(() => {
+        this.modalCtrl.dismiss({
+          'dismissed': true
+        });
+      })
+    ).pipe(
         takeUntil(this.destroy$),
-      )
-      .subscribe();
+      ).subscribe();
   }
 
 }
