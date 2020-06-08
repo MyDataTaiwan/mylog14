968bfab6bb12aa39fe855fbe304eeaf285b6e4d4
Sean Wu <seanwu1105@gmail.com>
2020-06-08 11:21:58 +0800

    Add setting tab and config routing.

---
+++ src/app/tabs/settings/settings.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { SettingsPageRoutingModule } from './settings-routing.module';
+import { MainHeaderModule } from '../../core/components/main-header/main-header.module';
+
+import { SettingsPage } from './settings.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    SettingsPageRoutingModule,
+    MainHeaderModule
+  ],
+  declarations: [SettingsPage]
+})
+export class SettingsPageModule { }
