d523af6582e548df0127c54599ecce28df9b5d7a
DaYuan <sduum82@gmail.com>
2020-06-16 16:34:04 +0800

    WIP

--- src/app/daily/daily-overview/daily-overview.component.ts
+++ src/app/daily/daily-overview/daily-overview.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
+import { PickerController, ModalController, PopoverController, AlertController, LoadingController } from '@ionic/angular';
 import { AnimationItem } from 'lottie-web';
 import { AnimationOptions, } from 'ngx-lottie';
-import { Observable, Subject, timer } from 'rxjs';
-import { tap, map, takeUntil, switchMap, filter } from 'rxjs/operators';
+import { tap, map, takeUntil, switchMap, filter, delay, concatMap } from 'rxjs/operators';
 import { DataStoreService } from 'src/app/core/services/data-store.service';
-
+import { Observable, from, defer, forkJoin, Subject, concat, pipe, timer, zip, of } from 'rxjs';
+import { RecordFinishPage } from '../../core/components/record-finish/record-finish.page';
+import { ImgPopoverPage } from '../../core/pages/img-popover/img-popover.page';
 @Component({
   selector: 'app-daily-overview',
   templateUrl: './daily-overview.component.html',
@@ -25,8 +27,8 @@ export class DailyOverviewComponent implements OnInit, OnDestroy {
   // }
 
   // arry: [number, number][] = [[35, 36], [125, 184], [195, 246], [254, 306], [313, 365], [372, 420], [436, 490], [494, 548], [555, 611], [613, 670], [677, 734], [740, 792], [797, 850], [860, 902], [952, 922], [999, 1200]];
-  arry: [number, number][] = [[1, 3], [125, 184], [125, 246], [125, 306], [125, 365], [125, 420], [125, 490], [125, 548], [125, 611], [125, 670], [125, 734], [125, 792], [125, 850], [125, 902], [125, 922], [984, 1200],[35, 36]];
-  STFarry: [number, number][] = [ [1, 3], [245, 246], [305, 306], [364, 365], [419, 420], [489, 490], [547, 548], [610, 611], [669, 670], [733, 734], [791, 792], [849, 850], [901, 902], [921, 922], [984, 1200],[35, 36]];
+  arry: [number, number][] = [[1, 3], [125, 184], [125, 246], [125, 306], [125, 365], [125, 420], [125, 490], [125, 548], [125, 611], [125, 670], [125, 734], [125, 792], [125, 850], [125, 902], [125, 922], [984, 1200], [35, 36]];
+  STFarry: [number, number][] = [[1, 3], [245, 246], [305, 306], [364, 365], [419, 420], [489, 490], [547, 548], [610, 611], [669, 670], [733, 734], [791, 792], [849, 850], [901, 902], [921, 922], [984, 1200], [35, 36]];
 
   options: AnimationOptions = {
     // path: '/assets/lottie-animation.json',
@@ -48,6 +50,8 @@ export class DailyOverviewComponent implements OnInit, OnDestroy {
 
   constructor(
     public dataStore: DataStoreService,
+    private popoverController: PopoverController,
+    private modalCtrl: ModalController,
     private ngZone: NgZone
   ) {
     this.items$ = this.dataStore.overviewCards$
@@ -59,6 +63,65 @@ export class DailyOverviewComponent implements OnInit, OnDestroy {
   ngOnInit() {
 
   }
+ 
+  async open() {
+    const modal = await this.popoverController.create({
+      component: RecordFinishPage,
+      translucent: true,
+      componentProps: {  }
+      // componentProps: {type:"ok", bottomBar:"ture",comp:<RecordFinishPage> }
+
+    });
+    return await modal.present();
+  }
+
+  // async open() {
+  //   const modal = await this.popoverController.create({
+  //     component: RecordFinishPage,
+  //     translucent: true,
+  //     componentProps: {type:"ok",title:"兌換成功" ,bottomBar:"ture",comp:<RecordFinishPage> }
+  //   });
+  //   return await modal.present();
+  // }
+
+  async show() {
+    // const modal = await this.popoverController.create({
+    //   component: RecordFinishPage,
+    //   translucent: true,
+    //   // componentProps: { "z" }
+    // });
+    // return await modal.present();
+
+     this.popoverController.create({
+       component:ImgPopoverPage,
+     showBackdrop:false
+    }).then((popoverElement)=>{
+     popoverElement.present();
+     })
+ 
+  }
+
+ 
+  private displayPopoverForDuration(popover: HTMLIonPopoverElement, seconds: number) {
+    return from(popover.present())
+      .pipe(
+        delay(seconds * 1000),
+        switchMap(() => popover.dismiss()),
+      );
+  }
+  // showRecordFinish() {
+  //   return defer(() => this.popoverController.create({
+  //     component: RecordFinishPage,
+  //     translucent: true,
+  //   }))
+  //     .pipe(
+  //       switchMap(popover => forkJoin([
+  //         this.displayPopoverForDuration(popover, 0.5),
+  //         this.onPopoverDismissNavigateBack(popover),
+  //       ])),
+  //     );
+  // }
+
 
   ngOnDestroy() {
     this.destroy$.next();
@@ -84,15 +147,15 @@ export class DailyOverviewComponent implements OnInit, OnDestroy {
 
   private startCountdown(day: number) {
     const idx = (day > 14) ? 15 : day + 1; // The animation array has +1 offset
-    this.WhatIsItToday=idx;
+    this.WhatIsItToday = idx;
     this.animationPlay(idx);
     // return timer((idx==15)? idx* 1 :idx*1000)
 
-    if (idx!=15){
-      return timer(idx*1000)
-      .pipe(
-        tap(() => this.animationStopOnDay(idx)),
-      );
+    if (idx != 15) {
+      return timer(idx * 1000)
+        .pipe(
+          tap(() => this.animationStopOnDay(idx)),
+        );
     }
   }
 
