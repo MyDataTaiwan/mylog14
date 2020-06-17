7ca2d45b16497217afb933b32991a53d2c51e7ca
DaYuan <sduum82@gmail.com>
2020-06-17 11:59:32 +0800

    add select mode

--- src/app/core/components/record-finish/record-finish.page.ts
+++ src/app/core/components/record-finish/record-finish.page.ts
@@ -11,17 +11,23 @@ export class RecordFinishPage implements OnInit {
   @Input() showBar: boolean;
   @Input() type: string;
   @Input() text: string;
+  @Input() Select: boolean =false;
+
   isShow=false
   constructor(
     private popoverCtrl: PopoverController,
   ) { }
 
   ngOnInit() {
+    if (this.Select==true){
+      this.isShow=true;
+    }
   }
   confirm(){
     this.type=="confirm";
     return true;
   }
+  cancel(){}
   async closePopover() {
     const onClosedData: string = "Wrapped Up!";
     await this.popoverCtrl.dismiss(onClosedData);
