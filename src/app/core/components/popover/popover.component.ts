7e8c3da29afa3d3226e453c72f9ce531196961b8
James Chien <shc261392@gmail.com>
2020-06-18 06:08:18 +0800

    Implement popover service/component props
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/core/components/popover/popover.component.ts
+++ src/app/core/components/popover/popover.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { PopoverButtonSet, PopoverIcon } from '../../services/popover.service';
 
 @Component({
   selector: 'app-popover',
@@ -7,8 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopoverComponent implements OnInit {
 
-  constructor() { }
+  @Input() i18nTitle: string;
+  @Input() icon: PopoverIcon;
+  @Input() i18nMessage?: string;
+  @Input() i18nExtraMessage?: string;
+  @Input() buttonSet?: PopoverButtonSet;
+  @Input() onConfirm?: () => {};
+  @Input() onCancel?: () => {};
+  public iconPath: string;
 
-  ngOnInit() {}
+  constructor(
+    private popoverCtrl: PopoverController,
+  ) { }
+
+  ngOnInit() {
+    this.iconPath = this.getIconPath(this.icon);
+  }
+
+  confirm(): void {
+    if (this.onConfirm) {
+      this.onConfirm();
+    }
+    this.popoverCtrl.dismiss();
+  }
+
+  cancel(): void {
+    if (this.onCancel) {
+      this.onCancel();
+    }
+    this.popoverCtrl.dismiss();
+  }
+
+  getIconPath(icon: PopoverIcon): string {
+    return `../../../../assets/alert/Alert_icon_${icon}.svg`;
+  }
 
 }
--- src/app/core/services/popover.service.ts
+++ src/app/core/services/popover.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, ComponentRef } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { Injectable } from '@angular/core';
 import { Observable, defer } from 'rxjs';
 import { PopoverComponent } from '../components/popover/popover.component';
-import { switchMap, mergeMap, filter, delay } from 'rxjs/operators';
+import { PopoverController } from '@ionic/angular';
+import { switchMap, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,26 +13,56 @@ export class PopoverService {
     private popoverCtrl: PopoverController,
   ) { }
 
-  showPopover() {
-    return this.createPopover()
+  showPopover(componentProps: PopoverProps, dismissTime?: number): Observable<any> {
+    return this.createPopover(componentProps)
       .pipe(
-        switchMap(popover => this.presentPopover(popover)),
+        switchMap(popover => this.presentPopover(popover, dismissTime)),
       );
   }
 
-  private createPopover(componentProps = {}): Observable<HTMLIonPopoverElement> {
+  private createPopover(componentProps: PopoverProps): Observable<HTMLIonPopoverElement> {
     return defer(() => this.popoverCtrl.create({
       component: PopoverComponent,
       componentProps,
+      animated: false,
     }));
   }
 
-  private presentPopover(popover: HTMLIonPopoverElement, dismissTime?: number): Observable<boolean> {
-    return defer(() => popover.present())
+  private presentPopover(popover: HTMLIonPopoverElement, dismissTime?: number): Observable<any> {
+    const manualDismiss$ = defer(() => popover.present())
       .pipe(
-        filter(() => !(dismissTime == null)),
+        switchMap(() => popover.onDidDismiss()),
+      );
+    const autoDismiss$ = defer(() => popover.present())
+      .pipe(
         delay(dismissTime),
         switchMap(() => popover.dismiss()),
       );
+    return (dismissTime) ? autoDismiss$ : manualDismiss$;
   }
+}
+
+export const enum PopoverIcon {
+  CHECK = 'check',
+  CONFIRM = 'confirm',
+  EMPTY = 'empty',
+  FAIL = 'fail',
+  LOADING = 'loading',
+  SORRY = 'sorry',
+  VERIFICATION = 'verification',
+}
+
+export const enum PopoverButtonSet {
+  CONFIRM = 'confirm',
+  NONE = 'none', // NOT IMPLEMENTED
+}
+
+export interface PopoverProps {
+  i18nTitle: string;
+  icon: PopoverIcon;
+  i18nMessage ?: string;
+  i18nExtraMessage ?: string;
+  buttonSet ?: PopoverButtonSet;
+  onConfirm ?: () => { };
+  onCancel ?: () => { };
 }
