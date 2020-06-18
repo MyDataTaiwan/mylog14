d37cff5ed376d4b083ef6fc32621b13ede5e5e7b
James Chien <shc261392@gmail.com>
2020-06-18 16:53:15 +0800

    Remove debug codes
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/core/services/qr-scanner.service.ts
+++ src/app/core/services/qr-scanner.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ShopScannerComponent } from '../components/shop-scanner/shop-scanner.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QrScannerService {
 
-  constructor() { }
+  constructor(
+    private modalCtrl: ModalController,
+  ) { }
+
+  // TODO: Reconsider how to manage the shop-scanner and coupon UI
+  async showShopScanner() {
+    const modal = await this.modalCtrl.create({
+      backdropDismiss: false,
+      component: ShopScannerComponent,
+    });
+    await modal.present();
+    const { data } = await modal.onWillDismiss();
+    return Promise.resolve(data);
+  }
 }
