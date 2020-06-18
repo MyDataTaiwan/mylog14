6855662a3942c7f1d7b9ad50d4bd0975e021768f
James Chien <shc261392@gmail.com>
2020-06-18 11:00:51 +0800

    Remove deprecated debug display of appVersion
    - App version are now shown in settings tab, so the hidden toast in
    main-header is no longer needed
    
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/core/components/main-header/main-header.component.ts
+++ src/app/core/components/main-header/main-header.component.ts
@@ -1,8 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ToastController } from '@ionic/angular';
-import { defer, Subject } from 'rxjs';
-import { buffer, debounceTime, filter, map, switchMap, tap } from 'rxjs/operators';
-import { version } from '../../../../../package.json';
 
 
 @Component({
@@ -13,46 +9,9 @@ import { version } from '../../../../../package.json';
 export class MainHeaderComponent implements OnInit {
   @Input() headerTitle: string;
 
-  public appVersion = version;
-
-  titleClicks = 0;
-  private click = new Subject<boolean>();
-  click$ = this.click.asObservable()
-    .pipe(
-      buffer(this.click
-        .pipe(debounceTime(500))
-      ),
-      tap(stream => console.log('stream', stream)),
-      map(stream => stream.length),
-      filter(length => length >= 3),
-      switchMap(() => this.showAppVersion(this.appVersion)),
-    );
-
-  constructor(
-    private toastCtrl: ToastController,
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  onClickTitle() {
-    this.click.next(true);
-  }
-
-  showAppVersion(appVersion: string) {
-    return defer(() => (this.toastCtrl.create({
-      message: 'App version: ' + appVersion,
-      position: 'top',
-      color: 'secondary',
-      cssClass: 'toast',
-      buttons: [{
-        text: 'Dismiss',
-        role: 'cancel',
-      }]
-    })))
-      .pipe(
-        switchMap(toast => toast.present()),
-      );
   }
 
 }
