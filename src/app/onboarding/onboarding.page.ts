eea1f00755f85b6eb4d7eb6bdb8119eac0986f88
Sean Wu <seanwu1105@gmail.com>
2020-06-21 00:04:09 +0800

    Implement signup when finshing onboarding slides.

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -1,8 +1,11 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { PrivateCouponService } from '@numbersprotocol/private-coupon';
 import { Subject } from 'rxjs';
-import { first, map, switchMap } from 'rxjs/operators';
+import { first, map, switchMap, tap } from 'rxjs/operators';
 import { DataStoreService } from '../core/services/data-store.service';
 import { TranslateConfigService } from '../core/services/translate-config.service';
 
@@ -13,32 +16,48 @@ import { TranslateConfigService } from '../core/services/translate-config.servic
 })
 export class OnboardingPage implements OnDestroy {
 
-  private destroy$ = new Subject();
-
   onboardingForm = this.formBuilder.group({
     email: ['', [Validators.email, Validators.required]],
     agreeTermsAndConditions: [false, Validators.requiredTrue]
   });
+  confirmButtonEnabled = true;
+
+  private destroy$ = new Subject();
 
   constructor(
     public translateConfigService: TranslateConfigService,
     private formBuilder: FormBuilder,
     private router: Router,
-    private dataStoreService: DataStoreService
+    private toastController: ToastController,
+    private dataStoreService: DataStoreService,
+    private privateCouponService: PrivateCouponService
   ) { }
 
   onSubmit() {
     this.dataStoreService.userData$.pipe(
       first(),
+      tap(() => this.confirmButtonEnabled = false),
       map(userData => {
         userData.email = this.onboardingForm.controls.email.value;
         userData.newUser = false;
         return userData;
       }),
-      switchMap(userData => this.dataStoreService.updateUserData(userData))
+      switchMap(userData => this.dataStoreService.updateUserData(userData)),
+      switchMap(userData => this.privateCouponService.signup(userData.email))
     ).subscribe(() => {
       this.router.navigate(['/']);
+    }, (error: HttpErrorResponse) => {
+      this.confirmButtonEnabled = true;
+      console.error(error);
+      this.presentToast(error.error.reason || error.statusText);
     });
+  }
+
+  private presentToast(message: string) {
+    this.toastController.create({
+      message,
+      duration: 3000
+    }).then(toast => toast.present());
   }
 
   ngOnDestroy() {
