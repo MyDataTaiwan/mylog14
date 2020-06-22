83a098c611fc5a75f9090c3bb1898d319d0095e7
James Chien <shc261392@gmail.com>
2020-06-23 03:30:58 +0800

    Add loading for signup

--- src/app/onboarding/onboarding.page.ts
+++ src/app/onboarding/onboarding.page.ts
@@ -2,12 +2,13 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { ToastController } from '@ionic/angular';
+import { ToastController, LoadingController } from '@ionic/angular';
 import { PrivateCouponService } from '@numbersprotocol/private-coupon';
-import { Subject, forkJoin } from 'rxjs';
+import { Subject, forkJoin, defer, of } from 'rxjs';
 import { first, map, switchMap, tap } from 'rxjs/operators';
 import { DataStoreService } from '../core/services/data-store.service';
 import { TranslateConfigService } from '../core/services/translate-config.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-onboarding',
@@ -26,13 +27,15 @@ export class OnboardingPage implements OnDestroy {
 
   constructor(
     public translateConfigService: TranslateConfigService,
+    private readonly translate: TranslateService,
     private readonly formBuilder: FormBuilder,
     private readonly router: Router,
     private readonly toastController: ToastController,
     private readonly dataStoreService: DataStoreService,
-    private readonly privateCouponService: PrivateCouponService
+    private readonly privateCouponService: PrivateCouponService,
+    private readonly loadingCtrl: LoadingController,
   ) { }
-
+ 
   onSubmit() {
     const userData = this.dataStoreService.getUserData();
     this.confirmButtonEnabled = false;
@@ -40,7 +43,12 @@ export class OnboardingPage implements OnDestroy {
     userData.newUser = false;
     const updateUserData$ = this.dataStoreService.updateUserData(userData);
     const signup$ = this.privateCouponService.signup(userData.email);
-    forkJoin([updateUserData$, signup$])
+    const loading$ = this.presentLoading();
+    forkJoin([updateUserData$, signup$, loading$])
+      .pipe(
+        map(([u, s, l]) => l),
+        tap(([loading, _]) => loading.dismiss()),
+      )
       .subscribe(() => {
         this.router.navigate(['/']);
       }, (error: HttpErrorResponse) => {
@@ -59,6 +67,19 @@ export class OnboardingPage implements OnDestroy {
       message,
       duration: 3000
     }).then(toast => toast.present());
+  }
+
+  private presentLoading() {
+    return this.translate.get('description.registeringUser')
+      .pipe(
+        switchMap(msg => {
+          return defer(() => this.loadingCtrl.create({
+            message: msg,
+            duration: 10000,
+          }));
+        }),
+        switchMap(loading => forkJoin([of(loading), loading.present()])),
+      );
   }
 
   ngOnDestroy() {
