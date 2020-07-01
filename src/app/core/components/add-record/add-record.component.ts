0bd049157ad0fb54f35f73c91f80c3c4e8750061
James Chien <shc261392@gmail.com>
2020-07-01 10:44:03 +0800

    Implement add record
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/core/components/add-record/add-record.component.ts
+++ src/app/core/components/add-record/add-record.component.ts
@@ -1,11 +1,8 @@
-import { Location } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { PickerController, ModalController } from '@ionic/angular';
-import { PickerOptions } from '@ionic/core';
+import { ModalController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
-import { defer, forkJoin, from, Observable, of, Subject } from 'rxjs';
-import { delay, map, switchMap, take, takeUntil, tap } from 'rxjs/operators';
-import { Symptom } from '../../classes/symptom';
+import { defer, forkJoin, from, Observable, of, Subject, BehaviorSubject } from 'rxjs';
+import { delay, map, switchMap, take, takeUntil, tap, filter, first, mergeMap } from 'rxjs/operators';
 import { PopoverService, PopoverIcon } from '../../services/popover.service';
 import { LoadingService } from '../../services/loading.service';
 import { Record } from '../../classes/record';
@@ -20,47 +17,34 @@ import { FormService } from '../../services/form.service';
   styleUrls: ['./add-record.component.scss'],
 })
 export class AddRecordComponent implements OnInit, OnDestroy {
-  btCIntegerList = this.genIntArr(34, 40).map(x => x.toString());
-  btDecimalList = this.genIntArr(0, 9).map(x => `.${x}`);
-  btUnitList = ['°C'];
-  defaultBt = '-';
-  defaultBtUnit = '°C';
-  bt: string;
-  btUnit: string;
-  text = {
-    recorded: '',
-    ok: '',
-    pickerTitle: '',
-    cancel: '',
-  };
-  recorded$: Observable<string>;
-  cancel$: Observable<string>;
-  pickerTitle$: Observable<string>;
-  ok$: Observable<string>;
   destroy$ = new Subject();
-  record$: Observable<Record> = this.recordActionService.create(RecordPreset.COMMON_COLD);
+
+  private readonly record = new BehaviorSubject<Record>(new Record(Date.now()));
+  record$: Observable<Record> = this.record;
   recordFieldType = RecordFieldType;
+
+  private readonly edit = new Subject<[RecordField, string]>();
+  edit$ = this.edit
+    .pipe(
+      switchMap(([field, templateName]) => this.editField(field, templateName)),
+      map(result => result.data),
+      filter(data => (data)),
+      tap(data => this.updateRecord(data)),
+    );
 
   constructor(
     private readonly loadingService: LoadingService,
-    private readonly pickerCtrl: PickerController,
     private readonly translate: TranslateService,
     private readonly popoverService: PopoverService,
     private readonly recordActionService: RecordActionService,
     private readonly formService: FormService,
     private readonly modalCtrl: ModalController,
   ) {
-    this.recorded$ = this.translate.get('title.recordSaved');
-    this.recorded$.subscribe((t: string) => this.text.recorded = t);
-    this.ok$ = this.translate.get('title.confirm');
-    this.ok$.subscribe((t: string) => this.text.ok = t);
-    this.pickerTitle$ = this.translate.get('title.addBodyTemperature');
-    this.pickerTitle$.subscribe((t: string) => this.text.pickerTitle = t);
-    this.cancel$ = this.translate.get('title.cancel');
-    this.cancel$.subscribe((t: string) => this.text.cancel = t);
   }
 
   ngOnInit() {
+    this.loadEmptyRecord();
+    this.edit$.subscribe();
   }
 
   ngOnDestroy() {
@@ -68,133 +52,68 @@ export class AddRecordComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  onToggleChanged() {
+  onClickEdit(field: RecordField, templateName: string) {
+    if (field.type !== this.recordFieldType.boolean) {
+      this.edit.next([field, templateName]);
+    }
   }
 
-  editRecord(field: RecordField, templateName: string) {
+  private editField(field: RecordField, templateName: string): Observable<any> {
     const formModel = {};
     formModel[field.name] = field.value;
-    this.popoverService.showPopover({
+    return this.popoverService.showPopover({
       i18nTitle: `preset.${templateName}.${field.name}`,
       i18nMessage: '',
       formModel,
       formFields: this.formService.createFormFieldsByRecordField(field, templateName),
-    }).subscribe();
-  }
-
-  showRecordSavedPopover(): Observable<any> {
-    return this.popoverService.showPopover({ i18nTitle: 'title.recordSaved', icon: PopoverIcon.CONFIRM }, 500)
-      .pipe(
-        tap(() => this.modalCtrl.dismiss()),
-        takeUntil(this.destroy$)
-      );
-  }
-
-  async presentBtPicker() {
-    const options: PickerOptions = {
-      id: 'add_bt',
-      buttons: [
-        {
-          text: this.text.cancel,
-          role: 'cancel'
-        },
-        {
-          // text: 'Add Body Temperature',
-          text: this.text.pickerTitle,
-          handler: (value: any) => {
-            return;
-          },
-          // cssClass: 'pickerClassDisable'
-          cssClass: 'test',
-        },
-        {
-          text: this.text.ok,
-          handler: (value: any) => {
-            this.bt = `${value.integer.value}${value.decimal.value}`;
-            this.btUnit = value.unit.value;
-          }
-        }
-      ],
-      columns: [
-        {
-          name: 'integer',
-          options: this.getColumnOptions(this.btCIntegerList)
-        },
-        {
-          name: 'decimal',
-          options: this.getColumnOptions(this.btDecimalList)
-        },
-        {
-          name: 'unit',
-          options: this.getColumnOptions(this.btUnitList)
-        }
-      ],
-      mode: 'ios'
-    };
-    const picker = await this.pickerCtrl.create(options);
-    await picker.present();
-  }
-
-  showAddingDataLoading(): Observable<HTMLIonLoadingElement> {
-    return this.translate.get('description.addingDataAndVerifiableInformation')
-      .pipe(
-        switchMap(msg => this.loadingService.showLoading(msg, 10000)),
-      );
-  }
-
-  getColumnOptions(column: string[]) {
-    const options = [];
-    column.forEach(x => {
-      options.push({ text: x, value: x });
     });
-    return options;
   }
 
-  onBodyTemperatureClick() {
-    this.presentBtPicker();
-  }
-
-  onCancelClick() {
+  onClickCancel() {
     this.modalCtrl.dismiss();
   }
 
-  onClearClick() {
-    this.resetPage();
+  onClickClear() {
+    this.loadEmptyRecord();
   }
 
-  onSubmitClick() {
+  onClickSubmit() {
     this.submitRecord().subscribe(() => { });
   }
 
   submitRecord(): Observable<any> {
-    return of([]);
-    // FIXME: It's a dirty hack to add/remove expand value for symptoms view
-    /*
-    this.symptoms.list = this.symptomsView;
-    this.symptoms.list.forEach((symptom: SymptomView) => delete symptom.expand);
     return forkJoin([
-      this.showAddingDataLoading(),
-      this.snapshotService.snapRecord(+this.bt, this.btUnit, this.symptoms),
+      this.loadingService.showLoading('description.addingDataAndVerifiableInformation', 10000),
+      this.recordActionService.save(this.record.getValue()),
     ])
       .pipe(
-        switchMap(([loadingElement, _]) => loadingElement.dismiss()),
-        switchMap(() => this.showRecordSavedPopover()),
+        mergeMap(([loading, _]) => loading.dismiss()),
+        mergeMap(() => this.showRecordSavedPopover()),
+        tap(() => this.modalCtrl.dismiss()),
         takeUntil(this.destroy$),
       );
-      */
   }
 
-  resetPage() {
-    
+  private loadEmptyRecord() {
+    this.recordActionService.create(RecordPreset.COMMON_COLD)
+      .pipe(
+        first(),
+        tap(record => this.record.next(record)),
+        takeUntil(this.destroy$),
+      ).subscribe();
   }
 
-  // Create an integer array [start..end]
-  private genIntArr(start: number, end: number, step = 1): number[] {
-    return Array.from({ length: end - start + 1 }, (_, k) => start + k);
+  private showRecordSavedPopover(): Observable<any> {
+    return this.popoverService.showPopover({ i18nTitle: 'title.recordSaved', icon: PopoverIcon.CONFIRM }, 500);
+  }
+
+  private updateRecord(data: any) {
+    const record = this.record.getValue();
+    for (const key of Object.keys(data)) {
+      record.setFieldValue(key, data[key]);
+    }
+    this.record.next(record);
   }
 
 }
 
-export interface SymptomView extends Symptom {
-  expand?: boolean;
-}
--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { defer, Observable } from 'rxjs';
 import { LoadingController } from '@ionic/angular';
 import { map, switchMap } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +11,19 @@ export class LoadingService {
 
   constructor(
     private readonly loadingCtrl: LoadingController,
+    private readonly translate: TranslateService,
   ) { }
 
-  showLoading(message: string, dismissTime: number = 0): Observable<HTMLIonLoadingElement> {
-    return this.createLoading(message, dismissTime)
+  showLoading(i18nMessage: string, dismissTime: number = 0): Observable<HTMLIonLoadingElement> {
+    return this.createLoading(i18nMessage, dismissTime)
       .pipe(
         switchMap(loading => this.presentLoading(loading)),
       );
   }
 
-  private createLoading(message: string, dismissTime: number): Observable<HTMLIonLoadingElement> {
+  private createLoading(i18nMessage: string, dismissTime: number): Observable<HTMLIonLoadingElement> {
     return defer(() => this.loadingCtrl.create({
-      message,
+      message: this.translate.instant(i18nMessage),
       duration: dismissTime,
     }));
   }
