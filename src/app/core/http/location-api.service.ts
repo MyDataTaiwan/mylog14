f843765746957ebb97a6a99ae1062e77254443c8
James Chien <shc261392@gmail.com>
2020-07-02 18:19:50 +0800

    Move reverse geocoding API call to LocationApi service
    Signed-off-by: James Chien <shc261392@gmail.com>

---
+++ src/app/core/http/location-api.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, switchMap, take } from 'rxjs/operators';
+
+import { DataStoreService } from '@core/services/store/data-store.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationApiService {
+
+  baseUrl = 'https://nominatim.openstreetmap.org';
+
+  constructor(
+    private readonly dataStore: DataStoreService,
+    private readonly httpClient: HttpClient,
+  ) { }
+
+  getReverseGeocoding(latitude: number, longitude: number): Observable<any> {
+    const endpoint = this.baseUrl + '/reverse';
+    return this.dataStore.userData$
+      .pipe(
+        take(1),
+        map(userData => this.createReverseGeocodingParams(latitude, longitude, userData.language)),
+        switchMap(params => this.httpClient.get<ReverseGeocoderResponse>(endpoint, { params })),
+        map(response => response.display_name),
+        catchError(error => {
+          console.error(error);
+          return of('');
+        })
+      );
+  }
+
+  private createReverseGeocodingParams(latitude: number, longitude: number, language: string): HttpParams {
+    return new HttpParams({
+      fromObject: {
+        format: 'json',
+        lat: `${latitude}`,
+        lon: `${longitude}`,
+        addressdetails: `0`,
+        zoom: `14`,
+        language,
+      }
+    });
+  }
+
+}
+
+interface ReverseGeocoderResponse {
+  place_id: number;
+  licence: string;
+  osm_type: string;
+  osm_id: number;
+  lat: string;
+  lon: string;
+  display_name: string;
+  boundingbox: string[];
+}
