ca4852e8cff79d400f4d363ac84a0bf4cd553291
James Chien <shc261392@gmail.com>
2020-07-13 16:40:06 +0800

    Fix summaryByDate data mapping
    Signed-off-by: James Chien <shc261392@gmail.com>

--- src/app/overview/summary-cards/summary-cards.component.ts
+++ src/app/overview/summary-cards/summary-cards.component.ts
@@ -1,6 +1,9 @@
 import { KeyValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
+import { map } from 'rxjs/operators';
+
+import { DailySummary } from '@core/interfaces/daily-summary';
 import { DataStoreService } from '@core/services/store/data-store.service';
 
 @Component({
@@ -9,9 +12,19 @@ import { DataStoreService } from '@core/services/store/data-store.service';
   styleUrls: ['./summary-cards.component.scss'],
 })
 export class SummaryCardsComponent implements OnInit {
-  summaryByDate$ = this.dataStore.summaryByDate$;
+  summaryByDateArray$ = this.dataStore.summaryByDate$
+    .pipe(
+      map(summaryByDate => {
+        let arr: SummaryByDateArrayElement[] = [];
+        Object.keys(summaryByDate).forEach(key => {
+          arr.push({ key, value: summaryByDate[key]});
+        });
+        arr = arr.sort((a, b) => +a.value.dayCount - +b.value.dayCount);
+        return arr;
+      })
+    );
 
-  keyDescOrder = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+  keyDescOrder = (a: KeyValue<string, string>, b: KeyValue<string, string>): number => {
     return a.key > b.key ? -1 : (b.key > a.key ? 1 : 0);
   }
 
@@ -21,4 +34,9 @@ export class SummaryCardsComponent implements OnInit {
 
   ngOnInit() {}
 
+}
+
+interface SummaryByDateArrayElement {
+  key: string;
+  value: DailySummary;
 }
