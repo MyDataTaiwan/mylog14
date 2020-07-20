f2d52630d4cb9af7868806becb73c7e463a681b3
James Chien <shc261392@gmail.com>
2020-07-21 04:23:18 +0800

    Add share tab
    Signed-off-by: James Chien <shc261392@gmail.com>

---
+++ src/app/tabs/share/share.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-share',
+  templateUrl: './share.page.html',
+  styleUrls: ['./share.page.scss'],
+})
+export class SharePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
