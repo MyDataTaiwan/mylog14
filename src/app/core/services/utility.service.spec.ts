b72f93cd83ea7c19cdf7ffed708bbe254baa3ee0
ethanwu155 <et.wuzz@gmail.com>
2020-08-26 00:33:10 +0800

    utility unit test added

--- src/app/core/services/utility.service.spec.ts
+++ src/app/core/services/utility.service.spec.ts
@@ -2,11 +2,20 @@ import { TestBed } from '@angular/core/testing';
 
 import { UtilityService } from './utility.service';
 
-describe('UtilityService', () => {
+fdescribe('UtilityService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: UtilityService = TestBed.get(UtilityService);
+    const service: UtilityService = TestBed.inject(UtilityService);
     expect(service).toBeTruthy();
+  });
+});
+
+fdescribe('gen', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should gen', () => {
+    const service: UtilityService = TestBed.inject(UtilityService);
+    expect(service.gen(1)).toBeDefined();
   });
 });
