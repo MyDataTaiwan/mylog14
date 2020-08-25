3c3fc3030e8fea3eac65b7f42851e0dbe6237d57
ethanwu155 <et.wuzz@gmail.com>
2020-08-26 00:29:57 +0800

    added preset unit tests

--- src/app/core/services/preset.service.spec.ts
+++ src/app/core/services/preset.service.spec.ts
@@ -1,12 +1,39 @@
 import { TestBed } from '@angular/core/testing';
+import { Record } from '../classes/record';
 
-import { PresetService } from './preset.service';
+import { PresetService, RecordPreset } from './preset.service';
 
-describe('PresetService', () => {
+fdescribe('PresetService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: PresetService = TestBed.get(PresetService);
+    const service: PresetService = TestBed.inject(PresetService);
     expect(service).toBeTruthy();
+  });
+});
+
+fdescribe('initRecordWithPreset with commonCold', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should initialize record with preset', () => {
+    const service: PresetService = TestBed.inject(PresetService);
+
+    const record = new Record(20190619);
+    const result = service.initRecordWithPreset(record, RecordPreset.COMMON_COLD);
+    expect(result.timestamp).toEqual(20190619);
+    expect(result.templateName).toEqual('commonCold');
+  });
+});
+
+fdescribe('initRecordWithPreset with heartfailure', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should initialize record with preset', () => {
+    const service: PresetService = TestBed.inject(PresetService);
+
+    const record = new Record(20201208);
+    const result = service.initRecordWithPreset(record, RecordPreset.HEART_FAILURE);
+    expect(result.timestamp).toEqual(20201208);
+    expect(result.templateName).toEqual('heartFailure');
   });
 });
